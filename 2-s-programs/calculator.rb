dfd1b51de7e6f0e699b23aeb8ca76b5a31fbbcc7
Richard Vu <20587893+richardtvu@users.noreply.github.com>
2022-04-13 21:02:48 -0400

    Refactor calculator to add input validation, abstract the get number functions, and calculation functions. Add a loop to repeat entire process.

--- 2-s-programs/calculator.rb
+++ 2-s-programs/calculator.rb
@@ -1,29 +1,99 @@
-puts "Calculator"
-
-puts "First number?"
-num1 = gets.chomp
-
-puts "Second number?"
-num2 = gets.chomp
-
-puts "What operation would you like to perform?"
-puts "1) add \n2) subtract \n3) multiply \n4) divide"
-operator = gets.chomp
-
-num1 = num1.to_i
-num2 = num2.to_i
-
-case (operator)
-when "1"
-  result = num1 + num2
-when "2"
-  result = num1 - num2
-when "3"
-  result = num1 * num2
-when "4"
-  result = num1.to_f / num2.to_f
-else
-  result = "Invalid operation. Input 1, 2, 3, or 4."
+def prompt(msg)
+  puts "=> #{msg}"
 end
 
-puts result
+def valid_number?(number)
+  if number == "0"
+    return true
+  end
+  if number.to_i == 0
+    return false
+  end
+  true
+end
+
+def get_valid_num(order)
+  loop do
+    prompt "#{order} number?"
+    num = gets.chomp
+    return num.to_i if valid_number?(num)
+    prompt "Invalid number. Try again"
+  end
+end
+
+def get_operator(operator_prompt, valid_operators)
+  loop do
+    prompt(operator_prompt)
+    operator = gets.chomp
+    return operator if valid_operators.include?(operator)
+    prompt("Invalid operation. Input 1, 2, 3, or 4.")
+  end
+end
+
+def do_calculation(operator, num1, num2)
+  case (operator)
+  when "1"
+    num1 + num2
+  when "2"
+    num1 - num2
+  when "3"
+    num1 * num2
+  when "4"
+    num1.to_f / num2.to_f
+  end
+end
+
+def get_name(name)
+  prompt "What's your name?"
+  loop do
+    name = gets.chomp
+    return name unless name.empty?
+    prompt "Input a valid name."
+  end
+end
+
+def keep_going?
+  prompt "Would you like to do another calculation?"
+  prompt "(Y to calculate again)"
+  again = gets.chomp.downcase
+  return true if again.start_with?("y")
+  false
+end
+
+def operation_to_message(operator)
+  case (operator)
+  when "1"
+    "Adding"
+  when "2"
+    "Subtracting"
+  when "3"
+    "Multiplying"
+  when "4"
+    "Dividing"
+  end
+end
+
+prompt "Calculator"
+name = get_name(name)
+prompt "Welcome #{name}!"
+
+loop do
+  num1 = get_valid_num("First")
+  num2 = get_valid_num("Second")
+
+  operator_prompt = <<-MSG
+What operation would you like to perform? 
+    1) add
+    2) subtract
+    3) multiply
+    4) divide
+  MSG
+  operator = get_operator(operator_prompt, %w(1 2 3 4))
+
+  prompt "#{operation_to_message(operator)} the two numbers..."
+  result = do_calculation(operator, num1, num2)
+  prompt("Result: #{result}")
+  break unless keep_going?
+end
+
+prompt("Bye bye!")
