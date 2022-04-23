95374b062dc41ee23badff3f32577df09f6b9cc4
Richard Vu <20587893+richardtvu@users.noreply.github.com>
2022-04-23 14:00:26 -0400

    Add case insensitive solution

---
+++ small-problems/easy-1/how_many_case_insensitive.rb
@@ -0,0 +1,20 @@
+def count_occurrences(array)
+  occurrences = {}
+
+  uniq_array = array.map(&:downcase).uniq
+  uniq_array.each do |element|
+    occurrences[element] =
+      array.map(&:downcase).count(element)
+  end
+
+  occurrences.each do |element, count|
+    puts "#{element} => #{count}"
+  end
+end
+
+vehicles = [
+  'Car', 'car', 'truck', 'car', 'SUV', 'truck',
+  'motorcycle', 'motorcycle', 'car', 'truck'
+]
+
+count_occurrences(vehicles)
