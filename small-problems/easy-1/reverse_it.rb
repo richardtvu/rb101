e840038e2968aa32cbc8511f11f96bccd660ba02
Richard Vu <20587893+richardtvu@users.noreply.github.com>
2022-04-24 08:31:06 -0400

    Add PEDAC for easy-1 reverse_it problem

---
+++ small-problems/easy-1/reverse_it.rb
@@ -0,0 +1,19 @@
+=begin 
+Reverse It - Part 1
+
+Problem: 
+- Input:  
+- Output: 
+- Requirements/Rules:
+- Clarifying questions: 
+
+
+Examples: 
+
+Data Structures: 
+
+Algorithm: 
+
+Code (with intent): 
+
+=end 
