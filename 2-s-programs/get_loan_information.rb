ca038150b44f11c1f4cf3fc8ab8024108a47a99e
Richard Vu <20587893+richardtvu@users.noreply.github.com>
2022-04-24 16:02:02 -0400

    Clean up comments and make variable names more concise.

--- 2-s-programs/get_loan_information.rb
+++ 2-s-programs/get_loan_information.rb
@@ -1,17 +1,16 @@
-# User Input Component of Mortgage Loan Calculator
-# This program will help me separate and test out the user input and validation component from the rest of the program.
+# Mortgage Loan Calculator
 
 require 'yaml'
 MESSAGES = YAML.load_file('loan_calculator_messages.yml')
 
-# This program supports English, 'en', for now. 
-LANGUAGE = 'en' 
+# This program supports English, 'en', for now.
+LANGUAGE = 'en'
 
-MIN_AMOUNT = 0.0 
+MIN_AMOUNT = 0.0
 MIN_TOTAL_DURATION_MONTHS = 1.0
 MONTHS_IN_YEAR = 12
 
-# Methods 
+# Methods
 
 def messages(message, lang=LANGUAGE)
   MESSAGES[lang][message]
@@ -59,50 +58,51 @@ def get_valid_number(request_msg, error_msg)
   end
 end
 
-def get_monthly_rate(annual_percentage_rate)
+def get_monthly_rate
+  annual_percentage_rate = get_valid_number("apr_msg", "apr_err_msg")
   annual_percentage_rate / MONTHS_IN_YEAR / 100.0
 end
 
-# MAIN
-
-# GET user input for the loan_amount, annual_percentage_rate, loan_duration_years. Candidate for refactoring.
-# Validate each input:
-
-loan_amount = get_valid_number("loan_msg", "loan_err_msg")
-
-# GET the annual_percentage_rate, in % (e.g. 5 is 5%)
-annual_percentage_rate = get_valid_number("apr_msg", "apr_err_msg")
-
-# Convert annual_percentage_rate into decimal.
-monthly_interest_rate = get_monthly_rate(annual_percentage_rate)
-
-def get_total_duration_months
+def get_total_dur_mos
   years = get_valid_number("loan_yr_msg", "yr_err_msg")
   months = get_valid_number("loan_mo_msg", "mo_err_msg")
   months + years * MONTHS_IN_YEAR
-end 
+end
 
-total_duration_months = get_total_duration_months
+# Monthly loan payment. 
+def get_mo_payment(loan_amt, mo_int_rate, total_dur_mos)
+  int_rate_mod = 1 - (1 + mo_int_rate)**(-total_dur_mos)
+  mo_pay_mod = mo_int_rate / int_rate_mod
+  loan_amt * mo_pay_mod
+end
+# MAIN
 
+loan_amt = get_valid_number("loan_msg", "loan_err_msg")
+monthly_interest_rate = get_monthly_rate
+total_dur_mos = get_total_dur_mos
 
-# # At this point, we've a valid loan: consisting of loan_amount, annual_percentage_rate, loan_duration_years, loan_duration_months.
+monthly_payment = get_mo_payment(loan_amt, monthly_interest_rate, total_dur_mos)
+
+# At this point, we've a valid loan: consisting of
+# loan_amt, 
+# annual_percentage_rate, 
+# loan_duration_years, 
+# loan_duration_months.
 
 loan = {}
-loan[:loan_amount] = loan_amount
-loan[:monthly_interest_rate] = monthly_interest_rate
-loan[:total_duration_months] = total_duration_months
+loan[:loan_amt] = loan_amt
+loan[:mo_int_rate] = monthly_interest_rate
+loan[:total_dur_mos] = total_dur_mos
 
 # Output hash
 loan.each do |key, value|
   puts "#{key} => #{value}"
 end
 
-interest_rate_mod = 1 - (1 + monthly_interest_rate)**(-total_duration_months)
-monthly_payment_modifier = monthly_interest_rate / interest_rate_mod
-monthly_payment = loan_amount * monthly_payment_modifier
+# int_rate_mod =
+#   1 -
+#   (1 + monthly_interest_rate)**(-total_dur_mos)
+# mo_pay_mod = monthly_interest_rate / int_rate_mod
+# monthly_payment = loan_amt * mo_pay_mod
 
 puts monthly_payment
-
-# NEXT STEP... is to Refactor the get loops for getting the loan amount, monthly interest rate, and total loan duration. ....
-# DONE is better than perfect.
-# Remove the hash.
