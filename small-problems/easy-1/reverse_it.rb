90ed104e475673a5312462b8516c83788ee56c41
Richard Vu <20587893+richardtvu@users.noreply.github.com>
2022-04-25 20:12:58 -0400

    Add PEDAC for easy questions and tips

--- small-problems/easy-1/reverse_it.rb
+++ small-problems/easy-1/reverse_it.rb
@@ -2,18 +2,49 @@
 Reverse It - Part 1
 
 Problem: 
-- Input:  
-- Output: 
-- Requirements/Rules:
-- Clarifying questions: 
+- Input: String (made up of words or nothing?)
+- Output: String with words reversed
+- Requirements/Rules: 
+  - Words will remain in the order they came, but the order will be reversed. 
+  - Implicit requirements: 
+    - Spaces are not preserved when there are no words. 
+    - Case must be preserved. 
+    - An empty string will be equal to an empty string. 
 
+- Clarifying questions: 
+  - Can there be more than one adjacent space separating words? If so, are these spaces to be preserved or replaced with a single space? 
+    - Let's say that there will only be _one_ space between words. 
 
 Examples: 
 
+puts reverse_sentence('Hello World') == 'World Hello'
+puts reverse_sentence('Reverse these words') == 'words these Reverse'
+puts reverse_sentence('') == ''
+puts reverse_sentence('    ') == '' # Any number of spaces results in ''
+
 Data Structures: 
+- Something to hold the different words (strings), that is easily reversible, and ordered --> An array!
+- Alternative? A hash? 
+
 
 Algorithm: 
 
-Code (with intent): 
+# START
+# GIVEN a string 
+# SET words = [] 
+# SET words = string.split(" ") # Splitting on the space. 
+# SET words = words.reverse.join(" ") # Reverse the array of words and then join the words into a string with a space between each word.  
+
+# END
 
 =end 
+# Code (with intent): 
+
+def reverse_sentence(string) 
+  string.split.reverse.join(" ") 
+end 
+
+puts reverse_sentence('Hello World') == 'World Hello'
+puts reverse_sentence('Reverse these words') == 'words these Reverse'
+puts reverse_sentence('') == ''
+puts reverse_sentence('    ') == '' # Any number of spaces results in ''
---
+++ small-problems/tips.md
@@ -0,0 +1,10 @@
+# Tips
+
+Output
+Return 
+Mutation - return the original object, but changed. 
+
+Use precise language (e.g. return value) + results 
+Keep the code separate from the algorithm (clean) 
+Verbalize the expected return value of each step in a chained method. 
+Test testcases one at a time for psychological reasons, managing anxiety. 
