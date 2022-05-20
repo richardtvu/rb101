93a17bf59ff5e7b0419bed68df9294aa7d1fe459
Richard Vu <20587893+richardtvu@users.noreply.github.com>
2022-05-20 07:52:36 -0400

    Updated leap year solution.

--- small-problems/easy-4/leap_years.rb
+++ small-problems/easy-4/leap_years.rb
@@ -1,102 +1,72 @@
-=begin
-# Feedback
+=begin 
+Leap Years 
+https://launchschool.com/exercises/f44b8f8b
 
-- Spend more time on the problem understanding phase: 
-  - You mixed up the Julian Calendar with the Gregorian calendar. 
-  - Next time verify the definitions of the terms with the interviewer.
-    e.g. "The Julian calendar is any year before 1752 or after? Just to clarify I'm reading the problem correctly."
+# Problem
 
 
 
-----
 
-A continuation of the previous exercise.
+- Input: Integer (>0)
+- Output: Boolean (true if leap, else false)
+- Requirements: 
+  - Gregorian = 1752 and onward  
+    - Leap years are divisible by 400 OR 
+    divisible by 4 and not 100. 
+  - Julian = 0-1751
+    - Leap years are divisible by 4. 
 
-The British Empire adopted the Gregorian Calendar in 1752, which was a leap year. Prior to 1752, the Julian Calendar was used. Under the Julian Calendar, leap years occur in any year that is evenly divisible by 4.
+- Questions: 
+  - Max year? Infinity. 
+  - Input data type? Integers or are floats acceptable too? 
+    - Integers only. 
 
-Using this information, update the method from the previous exercise to determine leap years both before and after 1752.
+# Data Structure
+- Intermediate? None
 
-P: 
-- Input: integer 
-- Output: boolean 
-- Requirement:
-  - True if leap year, else false. 
-  - Use the old rules for leap years when the year is earlier than 1752
-  - When the year is 1752 or later, then I just return true if I can by divide by 4
-  - Minimum Year: 0
-  - 0 is not leap year. 
-Examples: 
+# Algorithm
 
-leap_year?(1752) == true 
-leap_year?(1750) == false 
-leap_year?(0) == false 
-leap_year(2000) == true  
-leap_year?(2016) == true
-leap_year?(2015) == false
-leap_year?(2100) == false
-leap_year?(2400) == true
-leap_year?(240000) == true
-leap_year?(240001) == false
-leap_year?(2000) == true
-leap_year?(1900) == false
-leap_year?(1752) == true
-leap_year?(1700) == true
-leap_year?(1) == false
-leap_year?(100) == true
-leap_year?(400) == true
+If year is gregorian, then calculate leap year using gregorian rules. 
+Otherwise, calculate using Julian rules. 
+Return result. 
+# -------------------------------------- Code -------------------------------
 
-Data/Algo: 
-- Intermediate: Integer
-- Output: Boolean 
+=end 
 
+GREGORIAN_ADOPTION_YEAR = 1752 
 
-High-level:
-Make sure year is not 0. 
-Return based on the old leap year criteria for years before 1752. 
-Return based on the new leap criteria for years on or after 1752. 
-
-Low-level 
-GIVEN a integer 
-IF the integer is 0, return false. 
-IF the integer is between 1 and 1751
-  Return the value of the call to the the old leap year function 
-ELSE 
-  Return the result of getting the remainder of dividing the year by 4. 
-
-=end
-
-def old_leap_year?(year)
-  return false if year < 4
-    if year % 4 == 0
-      if year % 100 == 0
-        return false unless year % 400 == 0
-      end
-      return true
-    end
-    return false
-  end
-
-
-def leap_year?(year) 
-  return false if year == 0
-  if !year.between?(1, 1751) # For Julian calendar. 
-    return old_leap_year?(year)
-  else  
-     return year % 4 == 0  
-  end 
+def is_gregorian_year?(year) 
+  year >= GREGORIAN_ADOPTION_YEAR
 end 
 
-p leap_year?(2015)   == false
-p leap_year?(240001) == false
-p leap_year?(1)     == false
+def gregorian_leap_year?(year)
+  return true if year % 400 == 0 
+  return true if year % 4 == 0 && year % 100 != 0
+  false 
+end 
+
+def julian_leap_year?(year) 
+  return year % 4 == 0 
+end 
+
+def leap_year?(year) 
+  return gregorian_leap_year?(year) if is_gregorian_year?(year)
+  julian_leap_year?(year) 
+end 
+
+
 p leap_year?(2016) == true
+p leap_year?(2015) == false
 p leap_year?(2100) == false
-# p leap_year?(2400) == true
-# p leap_year?(240000) == true
-# p leap_year?(2000) == true
-# p leap_year?(1900) == false
-# p leap_year?(1752) == true
-# p leap_year?(1700) == false
-# p leap_year?(100) == false
-# p leap_year?(400) == true  
+p leap_year?(2400) == true
+p leap_year?(240000) == true
+p leap_year?(240001) == false
+p leap_year?(2000) == true
+p leap_year?(1900) == false
+p leap_year?(1752) == true
+p leap_year?(1700) == true
+p leap_year?(1) == false
+p leap_year?(100) == true
+p leap_year?(400) == true
+
 
