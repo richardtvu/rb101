651bd57edf66eba476f4bbe21bb7e5f929810563
Richard Vu <20587893+richardtvu@users.noreply.github.com>
2022-05-22 08:51:19 -0400

    Add attempt and revision  to easy 4.8

--- small-problems/easy-4/str_to_signed_num.rb
+++ small-problems/easy-4/str_to_signed_num.rb
@@ -1,6 +1,6 @@
 def make_ints(chars_arr)
   ints = {
-    '0' => 0, '1' => 1, '2' => 2, '3' => 3, '4' => 4, 
+    '0' => 0, '1' => 1, '2' => 2, '3' => 3, '4' => 4,
     '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9
   }
   chars_arr.map { |char| ints[char] }
@@ -15,25 +15,51 @@ def string_to_integer(digits_str)
   sum_arr.first
 end
 
-
-p string_to_integer('4321') == 4321
-p string_to_integer('570') == 570
-=begin 
+=begin
 Input: String of digits
-Output: Integer 
-- Requirements: 
-  - If the string has a + or no sign, return a positive number. 
-  - If the string has a -, return a negative number. 
-  - Input is always valid. 
-  - No built-in conversion methods. 
+Output: Integer
+- Requirements:
+  - If the string has a + or no sign, return a positive number.
+  - If the string has a -, return a negative number.
+  - Input is always valid.
+  - No built-in conversion methods.
 
-Data Structure: 
-- Array 
+Data Structure:
+- Array
 
-Algorithm: 
+Algorithm:
 Given a string containing a valid number, str.
-If there is a - sign, then return a negative number. 
-Else return a positive number. 
+If there is a - sign, then return a negative number.
+Else return a positive number.
 
+How to check - sign?
+Get the first charaacter in the string.
+If the first character is equal to '-', then return the a negative number.
 
-=end 
+=end
+
+# My attempt
+# SIGNED = ['+', '-']
+
+# def string_to_signed_integer(num_str)
+#   first_char = num_str[0]
+#   if SIGNED.include?(first_char)
+#     num = string_to_integer()
+#     return -num if first_char == '-'
+#     return  num if first_char == '+'
+#   end
+# end
+
+# Revision after looking at LS solution. I like the case statement here for clarity and organization.
+
+def string_to_signed_integer(num_str)
+  case num_str[0]
+  when '-' then -string_to_integer(num_str[1..-1])
+  when '+' then  string_to_integer(num_str[1..-1])
+  else           string_to_integer(num_str)
+  end
+end
+
+p string_to_signed_integer('4321') #== 4321
+p string_to_signed_integer('-570') #== -570
+p string_to_signed_integer('+100') #== 100
