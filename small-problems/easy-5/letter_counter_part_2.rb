b3f91408f6be1a114d4932e51849b1228bbcd6b9
Richard Vu <20587893+richardtvu@users.noreply.github.com>
2022-05-24 18:52:08 -0400

    Add some commentary

--- small-problems/easy-5/letter_counter_part_2.rb
+++ small-problems/easy-5/letter_counter_part_2.rb
@@ -26,6 +26,37 @@ def word_sizes(str)
   str.split.map { |word| word.delete('^A-Za-z').size }.tally
 end
 
+# Dr. Kaneyasu's Solution
+def word_sizes(str) 
+  str.split.each_with_object(Hash.new(0)) { |word, hsh| hsh[word.count "A-z"] +=1 } 
+end 
+
+# Commentary: 
+=begin 
+The `\W` used in the gsub method will filter out non-word characters. However, numbers and underscores are counted as word characters. The initial solution would fail with the edge case of '123abc'. 
+
+The `count` method will return the number of characters of the word that are in the range "A..z". This range includes non-letter characters, such as `[`, `\\`, `^`, etc.  Thus, you'd probably want to use count "a-zA-Z" if you only wanted alphabetical characters. 
+=end 
+
+# Annotated Dr. K's solution 
+def word_sizes(str) 
+  str       # Split the string into words. 
+  .split    # Invoke `each...` and pass in 
+            # a new Hash with default of 0. 
+  .each_with_object(Hash.new(0)) do 
+    |word, hsh| 
+    # 
+    hsh[word.count "A-z"] +=1 
+  end 
+end 
+
+
+
+
+
+
+
+
 p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 2 }
 p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 3 }
 p word_sizes("What's up doc?") == { 5 => 1, 2 => 1, 3 => 1 }
