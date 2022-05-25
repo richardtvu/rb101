f6b24016655dff27924d53e5c604524394cce7a0
Richard Vu <20587893+richardtvu@users.noreply.github.com>
2022-05-25 07:50:32 -0400

    Start daily dub

---
+++ small-problems/easy-5/daily_dub.rb
@@ -0,0 +1,26 @@
+=begin 
+ddaaiillyy ddoouubbllee
+https://launchschool.com/exercises/690b2900
+
+- Input: String 
+- Output: String 
+- Requirements: 
+  - The new String will remove consecutive duplicate characters, so each character will be different from the character before and after it. 
+  - Do not use #squeeze or #squeeze!
+  - Implicit Requirements: 
+    - An input of a string with 0 or 1 characters will have an output of that string value. 
+    - You can have more than one of the same character, as long as these characters are not next to each other. 
+
+DS/Algos: 
+- Array
+
+
+
+
+=end 
+
+
+
+
+
+# How would I use squeeze/squeeze! to simplify the solution? 
