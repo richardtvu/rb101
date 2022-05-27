39d2ce99b2d5c78af87b80852b581baabbcf8184
Richard Vu <20587893+richardtvu@users.noreply.github.com>
2022-05-27 17:48:34 -0400

    Start fib

---
+++ small-problems/easy-6/fib_index_by_length.rb
@@ -0,0 +1,14 @@
+# Fibonnaci Num Loc By Length
+=begin
+https://launchschool.com/exercises/1ba53710
+
+Fib Seq: 1, 1, 2, 3, 5 ... 
+- The first 2 numbers are 1.
+- Each subsequent number is the sum of the last two numbers. 
+
+Memo, array, dp? 
+
+- Input: Integer (length of fibonnaci number) 
+- Output: Integer (index of 1st fib num with given length)
+
+=end 
