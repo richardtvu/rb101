c7cfa5141b0d39fd01e7727b05d9bd224a55d61d
Richard Vu <20587893+richardtvu@users.noreply.github.com>
2022-05-30 10:41:23 -0400

    Add initial solution

---
+++ small-problems/easy-7/staggered_case.rb
@@ -0,0 +1,58 @@
+=begin
+Staggered Caps (Part 1)
+
+Input: String
+Output: String
+- Requirements:
+  - Output string has every other character capitalized, with
+  remaining characters lowercased.
+  - Non-letter characters will be the same in the output, but count
+  as characters when switching from upper and lowercase.
+
+Data Structure:
+- Array
+
+Algos:
+- Keep track of upper/lowercase turn.
+- Iterate through the string.
+  - When uppercase turn, capitalize the letter.
+  - Else downcase the letter.
+- Return the transformed string.
+=end
+
+# Initial Solution 
+def staggered_case(str)
+  uppercase = true
+  characters = str.chars.map do |char|
+    if uppercase
+      uppercase = false
+      char.upcase
+    else
+      uppercase = true
+      char.downcase
+    end
+  end
+  characters.join
+end
+
+# Refactored solution 
+def staggered_case(str)
+  uppercase = false # Set to true to have 1st letter be lowercase.
+  characters = str.chars.map do |char|
+    (uppercase = !uppercase) ? char.upcase : char.downcase
+  end
+  characters.join
+end
+
+# Further Exploration
+def staggered_case(str, first_letter_case = :upper)
+  first_letter_case == :upper ? uppercase = false : uppercase = true
+  characters = str.chars.map do |char|
+    (uppercase = !uppercase) ? char.upcase : char.downcase
+  end
+  characters.join
+end
+
+p staggered_case('I Love Launch School!') #== 'I LoVe lAuNcH ScHoOl!'
+p staggered_case('ALL_CAPS') == 'AlL_CaPs'
+p staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 NuMbErS'
---
+++ small-problems/easy-7/staggered_case_part2.rb
@@ -0,0 +1,55 @@
+=begin
+Staggered Caps (Part 2)
+
+Input: String
+Output: String
+- Requirements:
+  - Alternate upper/lowercasing letters.
+  - Ignore non-alphabetical characters when determining when to alternate upper/lowercasing.
+
+Iterate over the string,
+- Determine if the character is a letter.
+- Upper/lowcase letter as appropriate.
+- Flip the boolean for detemining upper/lowercase.
+- Return the mapped string.
+
+=end
+
+def letter?(char)
+  char.match(/[a-z]/i)
+end
+
+def upper_lower(char, uppercase)
+  uppercase ? char.upcase : char.downcase
+end
+
+def staggered_case_all_chars(str, first_letter_upper = true)
+  first_letter_upper ? uppercase = false : uppercase = true
+  characters = str.chars.map do |char|
+    (uppercase = !uppercase) ? char.upcase : char.downcase
+  end
+  characters.join
+end
+
+def staggered_case_only_letters(str, first_letter_upper = true)
+  first_letter_upper ? uppercase = false : uppercase = true
+  characters = str.chars.map do |char|
+    if letter?(char)
+      uppercase = !uppercase
+      upper_lower(char, uppercase)
+    else
+      char
+    end
+  end
+  characters.join
+end
+
+def staggered_case(str, first_letter_upper = true, only_count_letters = true)
+  return staggered_case_all_chars(str, first_letter_upper) unless only_count_letters
+
+  staggered_case_only_letters(str, first_letter_upper)
+end
+
+p staggered_case('I Love Launch School!') == 'I lOvE lAuNcH sChOoL!'
+p staggered_case('ALL CAPS') == 'AlL cApS'
+p staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 nUmBeRs'
