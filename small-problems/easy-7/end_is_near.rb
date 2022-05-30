6cd6d76ed9f2e096a50d52ccdff0a155071ddd70
Richard Vu <20587893+richardtvu@users.noreply.github.com>
2022-05-30 11:16:21 -0400

    Add initial solution + further exploration

---
+++ small-problems/easy-7/end_is_near.rb
@@ -0,0 +1,12 @@
+=begin 
+
+Split up the string into words. 
+Get the 2nd to last word. 
+
+=end
+def penultimate(string) 
+  string.split(" ")[-2]
+end 
+
+p penultimate('last word') == 'last'
+p penultimate('Launch School is great!') == 'is'
---
+++ small-problems/easy-7/phrase_middle.rb
@@ -0,0 +1,29 @@
+=begin 
+Further Exploration of The End is Near ... 
+https://launchschool.com/exercises/8332cd47
+
+- Input: String 
+- Output: String
+- Requirements: 
+  - Get the middle word of a sentence. 
+  - Words are sequences of non-blank characters. 
+- Edge cases? 
+  - Empty strings => Return empty string. 
+  - Strings with even # of words (i.e. no middle). => Return the first word of the two 
+  - String with one word. => Return string 
+
+=end 
+
+# Further Exploration 
+def middle_word(sentence) 
+  return sentence if sentence.size < 2
+  words = sentence.split(" ") #[middle]
+  words.size.even? ? middle = words.size / 2 - 1 : middle = words.size/2 
+  words[middle]
+end 
+
+p middle_word("Hello World") # => "Hello" 
+p middle_word("") 
+p middle_word("Sup")
+p middle_word("No way Jose") 
+p middle_word("Ya tu sabes guey") 
