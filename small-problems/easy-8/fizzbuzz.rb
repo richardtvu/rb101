a7a4d9c2f7cedc84f3772e309dd6026c0344446e
Richard Vu <20587893+richardtvu@users.noreply.github.com>
2022-06-01 17:41:45 -0400

    Fizzbuzz solution

--- small-problems/easy-8/fizzbuzz.rb
+++ small-problems/easy-8/fizzbuzz.rb
@@ -14,6 +14,31 @@ Data Structures?
 
 Algorithrm 
 
-Iterate through the numbers from 
 
+Initialize array to store the nums. 
+Iterate through the numbers from the begin_num to the end_num.
+- if number is divisible by 3 and 5, then append "FizzBuzz"
+- else if number is divisible by 3, then append "Fizz" 
+- else if number is divisible by 5, then append "Buzz" 
+- else append the number. 
+Combine the elements of array with , to delimit. 
+Print result. 
 =end 
+
+def divisible?(num, *divisors)
+  divisors.all? {|divisor| num % divisor == 0}
+end 
+
+def fizzbuzz(alpha, omega) 
+  fizzlin_buzzlin_arr = (alpha..omega).map do |num|
+    if    divisible?(num, 3, 5)  then "FizzBuzz"
+    elsif divisible?(num, 3)     then "Fizz"
+    elsif divisible?(num, 5)     then "Buzz"
+    else                               num 
+    end  
+  end 
+  
+  puts fizzlin_buzzlin_arr.join(', ')
+end 
+
+fizzbuzz(1, 15)
