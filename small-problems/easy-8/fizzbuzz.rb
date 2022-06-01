7534ed8f41f897bc0628d7801d1332c38af4e96e
Richard Vu <20587893+richardtvu@users.noreply.github.com>
2022-06-01 08:23:17 -0400

    Start fizzbuzz

---
+++ small-problems/easy-8/fizzbuzz.rb
@@ -0,0 +1,17 @@
+=begin
+
+- Input: 2 integers (starting num, ending num) 
+- Output: String (comma delimited) 
+- Requirements: 
+  - The output string will be the numbers in the range from the starting num to the ending num, inclusive*.
+  - Exceptions: 
+    - A number divisible by 3 and 5 will be replaced with "FizzBuzz. 
+    - A number divisible by 3 will be "Fizz"
+    - A number divisible by 5 will be "Buzz". 
+
+Data Structures? 
+- Array to store answers? 
+
+Algorithrm 
+
+=end 
