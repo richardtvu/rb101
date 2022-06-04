ce6bf42f33e953446a0153cba49556d5d438e8da
Richard Vu <20587893+richardtvu@users.noreply.github.com>
2022-06-03 20:31:10 -0400

    Start 1000 lights

---
+++ small-problems/medium_1/1000_lights.rb
@@ -0,0 +1,24 @@
+=begin
+- Input: Integer (number of switches, also the number of repetitions) 
+- Output: Array (the lights on)
+- Requirements:
+  - The light switches start in the off position. 
+  - On each repetition, the light switches flip state on multiples of the current repetition. 
+    e.g. 4th iteration: 4, 8, 12... 
+  - Repeat above process until we've reached the nth.
+  - Implicit Requirement: 
+    - The light is on when there is only a single light. 
+  - Questions:
+    - What happens when n is a negative number? 
+
+Examples: 
+- n = 1 => [1]
+- n = 5 => 
+
+[1, , , 4, ]
+
+=end 
+
+def thousand_lights(repetitions)
+
+end 
