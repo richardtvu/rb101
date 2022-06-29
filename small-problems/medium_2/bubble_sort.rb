fd1e3e094d328fbae973062519b5887456110993
Richard Vu <20587893+richardtvu@users.noreply.github.com>
2022-06-29 18:32:17 -0400

    Tail end optimization of Bubble Sort

---
+++ small-problems/medium_2/bubble_sort.rb
@@ -0,0 +1,106 @@
+=begin 
+Input: Unsorted array 
+Output: Sorted array 
+Requirements: 
+- Iterate through array:
+  - Compare current element with next element. 
+  - If the current element is greater than the second, swap the two elements. 
+- Repeat above until there are no more swaps to perform. 
+- THe sorting must be in-place, mutating the array. 
+- The array will have at least two elements. 
+- Arrays will contain elements of the same dat type? 
+Examples: [5,3] --> [3,5]
+
+Data Structure?
+- Array 
+
+ALgorithm: 
+- Iterate through each element in array... 
+  - If the element is greater than the next element, then swap the elements in-place. 
+- Repeat above until no more sorts are performed. 
+=end 
+
+def swap_curr_with_next!(arr, index) 
+  arr[index], arr[index+1] = arr[index+1], arr[index]
+  arr  
+end 
+
+def bubble_sort!(arr) 
+  loop do 
+    swapped = false 
+    arr.each_with_index do | element, ind | 
+      if (ind+1 < arr.size) && element > arr[ind+1] 
+        swap_curr_with_next!(arr, ind)
+        swapped = true 
+      end 
+    end 
+    break unless swapped 
+  end 
+  nil
+end 
+
+def bubble_sort!(arr) 
+  n = arr.length 
+  loop do 
+    swapped = false 
+    for i in (1..n-1)
+      if arr[i-1] > arr[i] 
+        arr[i-1], arr[i] = arr[i], arr[i-1]
+        swapped = true 
+      end 
+    end 
+    n -= 1 
+    break unless swapped 
+  end 
+end 
+
+def bubble_sort!(arr) 
+  n = arr.size 
+  loop do 
+    new_n = 0 
+    for i in (1..n-1) 
+      if arr[i-1] > arr[i]
+        arr[i], arr[i-1] = arr[i-1], arr[i]
+        new_n = i 
+      end 
+    end 
+    n = new_n
+    break if n <= 1
+  end 
+end 
+
+# Further Exploration - Tail End Optimization
+def swap_prev_with_curr!(arr, index) 
+  arr[index], arr[index-1] = arr[index-1], arr[index]
+  nil 
+end 
+
+def bubble_sort!(arr) 
+  n = arr.size 
+  until n <= 1
+    last_index_swapped = 0 
+    1.upto(n-1) do |index|
+      if arr[index-1] > arr[index] 
+        swap_prev_with_curr!(arr, index)
+        last_index_swapped = index 
+      end 
+    end 
+    n = last_index_swapped
+  end 
+  nil
+end 
+
+
+
+array = [5, 3]
+bubble_sort!(array)
+p array == [3, 5]
+
+array = [6, 2, 7, 1, 4]
+bubble_sort!(array)
+p array == [1, 2, 4, 6, 7]
+
+
+array = %w(Sue Pete Alice Tyler Rachel Kim Bonnie)
+bubble_sort!(array)
+p array == %w(Alice Bonnie Kim Pete Rachel Sue Tyler)
