816f041e26559eb7e29b6ec545f6e666f2ca776d
Richard Vu <20587893+richardtvu@users.noreply.github.com>
2022-07-23 03:33:55 -0400

    Make style corrections and turn off ABC/size cop.

--- lesson_6/tic_tac_toe.rb
+++ lesson_6/tic_tac_toe.rb
@@ -2,9 +2,9 @@ WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
                 [[1, 5, 9], [3, 5, 7]]              # diagonals
 
-INITIAL_MARKER  = ' '
-PLAYER_MARKER   = 'X'
-COMPUTER_MARKER = 'O'
+INITIAL_MARKER  = ' '.freeze
+PLAYER_MARKER   = 'X'.freeze
+COMPUTER_MARKER = 'O'.freeze
 
 def prompt(msg)
   puts "=> #{msg}"
@@ -12,22 +12,22 @@ end
 
 # 1. Display board.
 # Expects a board as a flattened hash, e.g. {1=>' ', ..., 9=>' '}.
-def display_board(brd)
+def display_board(brd) # rubocop:disable Metrics/AbcSize
   system 'clear'
   puts "You're a #{PLAYER_MARKER}. Computer is a #{COMPUTER_MARKER}."
-  puts ""
-  puts "     |     |"
+  puts ''
+  puts '     |     |'
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
-  puts "     |     |"
-  puts "-----+-----+-----"
-  puts "     |     |"
+  puts '     |     |'
+  puts '-----+-----+-----'
+  puts '     |     |'
   puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}"
-  puts "     |     |"
-  puts "-----+-----+-----"
-  puts "     |     |"
+  puts '     |     |'
+  puts '-----+-----+-----'
+  puts '     |     |'
   puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}"
-  puts "     |     |"
-  puts ""
+  puts '     |     |'
+  puts ''
 end
 
 def initialize_board(marker = INITIAL_MARKER)
@@ -48,7 +48,7 @@ def player_places_piece!(brd, player_marker = PLAYER_MARKER)
     square = gets.chomp.to_i
     break if empty_squares.include?(square)
 
-    puts "Choose an empty square..."
+    puts 'Choose an empty square...'
   end
 
   brd[square] = player_marker
@@ -57,7 +57,7 @@ end
 
 # Bonus #1: Improved "join"
 def joinor(squares_arr, delimiter = ', ', join_word = 'or')
-  arr = squares_arr[0..-1] # To avoid mutating original array.
+  arr = squares_arr[0..-1]
   case arr.size
   when 0 then ''
   when 1 then arr.first
@@ -76,10 +76,10 @@ def find_at_risk_square(line, board, marker)
 end
 
 def move_choice(brd, mode = :attack)
-  case (mode) 
+  case mode
   when :attack then marker = COMPUTER_MARKER
   when :defend then marker = PLAYER_MARKER
-  end 
+  end
   WINNING_LINES.each do |line|
     square = find_at_risk_square(line, brd, marker)
     return square if square
@@ -126,18 +126,18 @@ end
 # Bonus TTT #5 - Computer Turn Refinements
 def determine_order
   choices_hash = {
-    1 => :player, 2 => :computer, 3 => [:player, :computer].sample
+    1 => :player, 2 => :computer, 3 => %i(player computer).sample
   }
   prompt "Who goes first? Enter #{joinor(choices_hash.keys)}"
-  prompt "1) Player"
-  prompt "2) Computer"
-  prompt "3) Random"
+  prompt '1) Player'
+  prompt '2) Computer'
+  prompt '3) Random'
 
   loop do
     order = gets.chomp.to_i
     break choices_hash[order] if choices_hash[order]
 
-    prompt "Invalid choice..."
+    prompt 'Invalid choice...'
   end
 end
 
@@ -150,7 +150,7 @@ def place_piece!(board, current_player)
 end
 
 def alternate_player(current_player)
-  [:player, :computer].find { |player| player != current_player }
+  %i(player computer).find { |player| player != current_player }
 end
 
 # Bonus #6: Improved Game Loop
@@ -158,7 +158,7 @@ def make_moves!(board, current_player)
   loop do
     display_board(board)
     break if someone_won?(board) || board_full?(board)
-    
+
     place_piece!(board, current_player)
     current_player = alternate_player(current_player)
   end
@@ -171,18 +171,18 @@ def play
 
     make_moves!(board, determine_order)
 
-    if someone_won?(board) then 
+    if someone_won?(board)
       prompt "#{detect_winner(board)} won!"
     else
       prompt "It's a tie!"
     end
 
-    prompt "Play again? (y or n)"
+    prompt 'Play again? (y or n)'
     answer = gets.chomp
     break unless answer.downcase.start_with?('y')
   end
 
-  prompt "Thanks for playing Tic Tac Toe. Goodbye!"
+  prompt 'Thanks for playing Tic Tac Toe. Goodbye!'
 end
 
 play
