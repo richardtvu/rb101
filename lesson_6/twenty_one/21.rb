4df1563a1b55eb99529c6968d55b45f7d08637f5
Richard Vu <20587893+richardtvu@users.noreply.github.com>
2022-08-07 10:22:35 -0400

    Check in 21 v2 for peer code review and organize folder structure.

--- lesson_6/twenty_one/21.rb
+++ lesson_6/twenty_one/21.rb
@@ -1,206 +1,488 @@
-# Twenty One - A Card Game
+=begin
+Thanks to JD Fortune for kindly donating his design and code for
+displaying cards. Thanks to Amy D. for collaborating on deck
+design, code reviews, and other discussions to flesh out this code.
+=end
+require 'pry'
+require_relative 'message'
 
-CARDS = ((2..9).to_a + %w(Jack Queen King Ace)).freeze
-SUITS = %w(Clubs Diamonds Hearts Spades).freeze
+# ================== INITIALIZE CONSTANTS =====================
 
-def clear_screen
-  system('cls') || system('clear')
-end
+PARTICIPANT = {
+  id: nil,
+  name: '', role: 'Player', score: 0,
+  hand: nil, total: 0, busted: false
+}.freeze
+
+DEALER_NAME = 'Jack Durango Fortuna'.freeze
+
+SUITS = %w(♥ ♣ ♦ ♠).freeze
+VALUES = (2..9).to_a.concat(%w(J Q K A)).freeze
+ACE = 'A'.freeze
+HIDDEN_CARD = ['?', '?'].freeze
+
+MAX = 21 # The max total value a player can have before busting.
+HIT_LIMIT = 17 # The dealer must hit until their hand is worth
+# at least this value.
+
+TABLE_WIDTH = 71 # Number of characters needed to fit a 6 cards
+NUM_CARDS_PER_ROW = 6 # Max cards that can be displayed before
+# pretty-printing stops working.
+YES_OR_NO = %w(y n).freeze
+
+# ================== HELPER METHODS =====================
+
+# ------------------- Display Methods -------------------
 
 def prompt(msg)
-  puts "==> #{msg}\n\n"
+  puts "=> #{msg}"
 end
 
-# Disabled this cop because I'd rather not create a YML file
-# and load up the message from there.
-def welcome_player # rubocop:disable Metrics/MethodLength
+def clear_screen
+  system('clear') || system('cls')
+end
+
+# See 'message.rb'.
+def welcome_player(msg = welcome_message())
   clear_screen
-  msg = <<~MSG
-Rules of Twenty-One
-- You start with a normal 52-card deck consisting of the 4 suits
-(hearts, diamonds, clubs, and spades), and 13 values (2, 3, 4, 5,
-6, 7, 8, 9, 10, jack, queen, king, ace).
-
-- The goal of Twenty-One is to try to get as close to 21 as possible,
-without going over. If you go over 21, it's a "bust" and you lose.
-
-- Setup: the game consists of a "dealer" and a "player". Both
-participants are initially dealt 2 cards. The player can see their
-2 cards, but can only see one of the dealer's cards.
-
-- Cards 2-10 are worth their face value. Jack, Queen, and king
-are worth 10. Ace is worth 1 or 11.
-
-- Player turn: the player goes first, and can decide to either "hit"
-or "stay". A hit means the player will ask for another card. Remember
-that if the total exceeds 21, then the player "busts" and loses.
-The decision to hit or stay will depend on what the player's cards are
-and what the player thinks the dealer has. For example, if the dealer
-is showing a "10" (the other card is hidden), and the player has a "2"
-and a "4", then the obvious choice is for the player to "hit". The
-player can continue to hit as many times as they want. The turn is
-over when the player either busts or stays. If the player busts, the
-game is over and the dealer won.
-
-- Dealer turn: when the player stays, it's the dealer's turn. The
-dealer must follow a strict rule for determining whether to hit or
-stay: hit until the total is at least 17. If the dealer busts, then
-the player wins.
-MSG
-  prompt msg
+  puts msg
+  puts ''
 end
 
-# Credit: JD Fortune & Amy D for shuffle and deck initialization.
-def deck(suits = SUITS, cards = CARDS)
-  deck = suits.map do |suit|
-    [suit, cards.dup]
-  end.to_h
+def total_based_on_view(player, view)
+  show_cards?(player, view) ? player[:total] : '?'
+end
 
-  deck.keys.map.with_object([]) do |suit, new_deck|
-    new_deck << "#{deck[suit].pop}:#{suit}" until deck[suit].empty?
+def card_image(card) # rubocop:disable Metrics/AbcSize
+  cards = []
+  cards << '+---------+'
+  cards << ("#{'|'.ljust(8)}#{(card[1]).to_s.ljust(2)}|")
+  cards << '|         |'
+  cards << ('|'.ljust(5) + (card[0]).to_s + '|'.rjust(5))
+  cards << '|         |'
+  cards << ("|#{(card[1]).to_s.rjust(2)}#{'|'.rjust(8)}")
+  cards << '+---------+'
+  cards
+end
+
+def prettify!(cards)
+  cards.map! { |card| card_image(card) }
+end
+
+def split_into_groups!(num_cards_per_row, cards)
+  temp_cards = cards.dup
+  cards.pop(cards.size)
+
+  cards << temp_cards.pop(num_cards_per_row) until temp_cards.empty?
+
+  cards
+end
+
+def lay_cards_in_rows!(cards)
+  cards.map! do |row_of_cards|
+    row_of_cards.transpose.map { |a| a.join(' ') }
   end
 end
 
-def shuffle(deck)
-  deck.sample(deck.size)
-end
+def hide_dealer!(cards)
+  hand = [cards.first, HIDDEN_CARD]
+  cards.pop(cards.size)
 
-def deal!(deck, hand, num_cards = 1)
-  num_cards.times.with_object(hand) { hand << deck.pop }
-  nil
-end
-
-def deal_cards!(deck, player_cards, dealer_cards)
-  deal!(deck, player_cards, 2)
-  deal!(deck, dealer_cards, 2)
-end
-
-def value(card_symbol)
-  case card_symbol
-  when '2', '3', '4', '5', '6', '7', '8', '9', '10' then card_symbol.to_i
-  when 'Jack', 'Queen', 'King' then 10
-  when 'Ace' then 11
+  hand.each do |card|
+    cards << card
   end
+end
+
+def show_cards?(player, view)
+  return true unless view.downcase == 'player'
+
+  player?(player)
+end
+
+def line_up!(cards, num_cards_per_row = NUM_CARDS_PER_ROW)
+  split_into_groups!(num_cards_per_row, cards)
+  lay_cards_in_rows!(cards)
+end
+
+def display_cards(player, view = 'player')
+  cards = player[:hand].dup
+
+  hide_dealer!(cards) unless show_cards?(player, view)
+  prettify!(cards)
+  line_up!(cards)
+
+  puts cards
+end
+
+def display_boundary(word, width = TABLE_WIDTH,
+                     padding_symbol = '=', line_spacing = nil)
+  puts word.center(width, padding_symbol)
+  puts "\n\n\n" if line_spacing == :extra_space
+end
+
+def scores(players)
+  scores = players.map { |player| "#{player[:name]}: #{player[:score]}" }
+  scores.join(' | ')
+end
+
+def display_scores(players, width)
+  puts 'SCORES'.center(width)
+  puts ''.center(width, '-')
+  scores = scores(players)
+
+  puts "#{scores.center(width)}\n\n"
+end
+
+# 71 is the width of a full row of cards.
+def display_table(players, view = 'player', width = TABLE_WIDTH)
+  clear_screen
+  display_boundary('GAME')
+
+  players.each do |player|
+    total = total_based_on_view(player, view)
+
+    puts "\n#{player[:role]}: #{player[:name]}" \
+         "\n Total: #{total}"
+
+    display_cards(player, view)
+    puts "\n"
+  end
+
+  display_boundary('BOARD')
+  puts "\n\n"
+  display_scores(players, width)
+end
+
+# ------------------- Initialization Methods -------------
+
+def initialize_deck
+  SUITS.product(VALUES).shuffle
+end
+
+def request_name(player_num)
+  prompt "You're player #{player_num}. What's your name?"
+  name = nil
+  loop do
+    name = gets.chomp
+    break unless name.empty?
+
+    prompt 'Please enter at least one character.'
+  end
+  name
+end
+
+def initialize_player(player_num, test = nil)
+  name = if test == :test
+           "Test Player #{player_num}"
+         else
+           request_name(player_num)
+         end
+
+  player = PARTICIPANT.dup
+  player[:id] = player_num
+  player[:name] = name
+  player[:hand] = Array.new
+  player
+end
+
+def initialize_players(num_players = 1, test = nil)
+  if num_players < 1
+    prompt 'Number of players must be at least 1...'
+    return
+  end
+
+  players = []
+  1.upto(num_players) do |player_num|
+    players << initialize_player(player_num, test)
+  end
+
+  dealer = PARTICIPANT.dup
+  dealer[:id] = players.last[:id] + 1
+  dealer[:name] = DEALER_NAME
+  dealer[:role] = 'Dealer'
+  dealer[:hand] = Array.new
+  players << dealer
+end
+
+# ------------------- Decision Methods ------------------
+
+def player?(player)
+  player[:role].downcase == 'player'
+end
+
+def valid_choice?(choice, choices)
+  return false unless choice
+
+  choices.include?(choice.downcase)
+end
+
+def response(choices)
+  choice = nil
+  loop do
+    choice = gets.chomp
+    break if valid_choice?(choice, choices)
+
+    prompt "You may only enter '#{choices[0]}' or '#{choices[1]}'."
+  end
+  choice.downcase
+end
+
+def affirmative?(valid_choices)
+  response(valid_choices) == 'y'
+end
+
+def player_wants_to?(request)
+  prompt "Want to #{request}? Enter (y)es or (n)o."
+
+  affirmative?(YES_OR_NO)
+end
+
+def winner_of_game?(players)
+  sleep(0.5)
+  players.any? do |player|
+    player[:score] >= 5
+  end
+end
+
+# ---------------------- Deal Card Methods -----------------------
+
+def value(card)
+  case (card[1])
+  when 'A' then 11
+  when 'J', 'Q', 'K' then 10
+  else card[1].to_i
+  end
+end
+
+def update_total!(player, card)
+  return unless card
+
+  total = player[:total] + value(card)
+
+  # correct for Aces
+  player[:total] = adjust_total(total, num_aces(player[:hand]))
 end
 
 def num_aces(cards)
-  cards.count('Ace')
+  cards.select { |card| card[1] == 'A' }.count
 end
 
-def adjust_score(score, num_aces)
-  return score if num_aces < 1
+def adjust_total(total, num_aces)
+  return total if num_aces < 1
 
-  until score <= 21 || num_aces.zero?
-    score -= 10
-    num_aces -= 1
+  total -= (11 *  num_aces)
+  num_aces.times do
+    total += (total + 11) < MAX ? 11 : 1
   end
-  score
+  total
 end
 
-def score(cards)
-  faces = cards.map { |face| face.split(':').first }
-  score = faces.map { |face| value(face) }.sum
+def deal!(deck, player, num_cards = 1)
+  num_cards.times do
+    card = deck.pop
+    break unless card
 
-  adjust_score(score, num_aces(faces))
+    player[:hand] << card
+
+    update_total!(player, card)
+    player[:busted] = true if busted?(player)
+  end
 end
 
-def show_cards(player_cards, dealer_cards)
+def deal_cards!(deck, players, num_cards = 2)
+  players.each { |player| deal!(deck, player, num_cards) }
+
+  nil
+end
+
+def busted?(player)
+  player[:total] > 21
+end
+
+def all_busted?(players)
+  players[0...-1].all? { |player| player[:busted] }
+end
+
+def hit?(player)
+  case player[:role].downcase
+  when 'player'
+    player_wants_to? 'hit'
+  else
+    player[:total] < 17
+  end
+end
+
+def take_turn!(deck, players, player)
+  turn = player[:role].downcase
+  loop do
+    break if busted?(player)
+    return unless hit?(player)
+
+    deal!(deck, player)
+    sleep(1) if turn == 'dealer'
+    display_table(players, turn)
+  end
+  sleep(1)
+end
+
+def play_round!(players)
+  deck = initialize_deck
+
+  deal_cards!(deck, players)
+  display_table(players)
+
+  players[0...-1].each do |player|
+    prompt "Player #{player[:name]}, it's your turn."
+
+    take_turn!(deck, players, player)
+  end
+
+  display_table(players, 'dealer')
+
+  if all_busted?(players)
+    prompt(players.size == 2 ? 'Player busted!' : 'All players busted!')
+    sleep(1)
+    return
+  end
+
+  take_turn!(deck, players, players.last)
+end
+
+def best_total(players)
+  players.max do |player1, player2|
+    player1[:total] <=> player2[:total]
+  end[:total]
+end
+
+def all_tied?(players)
+  return false if players.size == 1
+
+  players.all? do |player|
+    player[:total] == best_total(players)
+  end
+end
+
+def player_with_best_total(players)
+  players.find do |player|
+    player[:total] == best_total(players)
+  end
+end
+
+def round_winner(players)
+  players = players.reject { |player| player[:busted] }
+  return player_with_best_total(players) unless all_tied?(players)
+
+  nil
+end
+
+def update_score!(round_winner)
+  return unless round_winner
+
+  round_winner[:score] += 1
+  sleep(0.5)
+  nil
+end
+
+def display_centered_message(msg, _width = TABLE_WIDTH)
+  puts msg.center(TABLE_WIDTH)
+  puts ''
+end
+
+def display_art(winner, player_win_art, dealer_win_art)
+  if player?(winner)
+    display_centered_message(player_win_art)
+  else
+    display_centered_message(dealer_win_art)
+  end
+end
+
+def display_end_game_art(winner)
+  display_art(winner, congrats_message, sad_dinosaur_message)
+end
+
+def show_art(phase, winner)
+  if phase == 'round'
+    display_art(winner, success_message, sad_face_message)
+  else
+    display_end_game_art(winner)
+  end
+end
+
+def display_result(players, winner, phase = 'round')
+  if phase == 'round'
+    clear_screen if phase == 'round'
+    display_scores(players, TABLE_WIDTH)
+  end
+
+  unless winner
+    display_centered_message("The #{phase} is a tie!")
+    return
+  end
+
+  msg = "#{winner[:role]} #{winner[:name]} " \
+        "is the winner of the #{phase}!\n\n"
+
+  display_centered_message(msg)
+  show_art(phase, winner)
+end
+
+def show_game_result(players, winner, width = TABLE_WIDTH)
   clear_screen
-  prompt 'Player'
-  prompt "#{player_cards}. Score: #{score(player_cards)}\n"
-  prompt 'Dealer'
-  prompt "#{dealer_cards}. Dealer's Score: #{score(dealer_cards)}\n"
+
+  display_boundary('END OF GAME', width, '=', :extra_space)
+  display_result(players, winner, 'game')
+  display_boundary('END OF GAME', width, '=', :extra_space)
 end
 
-def stay?(cards, turn_player)
-  case turn_player
-  when :player
-    response = nil
-    loop do
-      prompt 'Enter h/hit to hit or s/stay to stay'
-      response = gets.chomp.downcase
-      break if %w(h s hit stay).include?(response)
+def reset_round!(players)
+  players.each do |player|
+    player[:total] = 0
+    player[:busted] = false
+    player[:hand] = []
+  end
+  sleep(1)
+end
 
-      prompt 'Invalid response...'
+def play_game!
+  clear_screen
+  players = initialize_players
+
+  loop do
+    play_round!(players)
+    winner = round_winner(players)
+    update_score!(winner)
+
+    if winner_of_game?(players)
+      show_game_result(players, winner)
+      return
     end
-    %w(s stay).include?(response)
-  when :dealer
-    score(cards) > 16
+
+    display_result(players, winner)
+    reset_round!(players)
+    break unless player_wants_to? 'continue this game'
   end
 end
 
-def hit!(deck, hand)
-  deal!(deck, hand)
-end
-
-def busted?(hand)
-  score(hand) > 21
-end
-
-def take_turn!(deck, player_cards, dealer_cards, turn_player = :player)
-  hand = dealer_cards
-  visible_dealer_cards = hand
-
-  if turn_player == :player
-    hand = player_cards
-    visible_dealer_cards = [dealer_cards.first]
-  end
-
-  loop do
-    show_cards(player_cards, visible_dealer_cards)
-    break if stay?(hand, turn_player)
-
-    hit!(deck, hand)
-    break if busted?(hand)
-  end
-end
-
-def results(player_cards, dealer_cards)
-  show_cards(player_cards, dealer_cards)
-
-  player_score = busted?(player_cards) ? 0 : score(player_cards)
-  dealer_cards = busted?(dealer_cards) ? 0 : score(dealer_cards)
-
-  case (player_score <=> dealer_cards)
-  when -1 then 'Player lost!'
-  when 0 then 'Tie!'
-  when 1 then 'Player wins!'
-  end
-end
-
-def play_game!(deck, player_cards = [], dealer_cards = [])
-  deal_cards!(deck, player_cards, dealer_cards)
-
-  take_turn!(deck, player_cards, dealer_cards)
-
-  unless busted?(player_cards)
-    take_turn!(deck, player_cards, dealer_cards,
-               :dealer)
-  end
-
-  prompt results(player_cards, dealer_cards)
-end
-
-def play?
-  response = nil
-  loop do
-    prompt 'Enter y/yes to play or n/no to exit.'
-    response = gets.chomp.downcase
-    break if %w(y yes n no).include?(response)
-
-    prompt 'Invalid response...'
-  end
-  %w(y yes).include?(response)
+def good_bye
+  clear_screen
+  display_boundary('BYE!', TABLE_WIDTH, '=')
+  puts good_bye_message
+  display_boundary('BYE!', TABLE_WIDTH, '=')
 end
 
 def main
   welcome_player
 
-  deck = shuffle(deck())
+  return unless player_wants_to? 'play'
 
-  play_game!(deck) while play?
+  loop do
+    play_game!
 
-  prompt 'Good bye!'
+    sleep(1)
+    break unless player_wants_to? 'play a new game'
+  end
+
+  good_bye
 end
 
 main
